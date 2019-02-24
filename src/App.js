f7f57636cd5da530493bba93436801922e4b4027
juunone <juunone@gmail.com>
2019-02-24 11:42:05 +0900

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Empty from './images/star-empty.svg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <img src={Empty} className="" alt="empty" />
+          <img src={Empty} className="" alt="empty" />
+          <img src={Empty} className="" alt="empty" />
+          <img src={Empty} className="" alt="empty" />
+          <img src={Empty} className="" alt="empty" />
       </div>
     );
   }
